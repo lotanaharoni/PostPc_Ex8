71078a4872bb31479cfcb664f46c079929dc4bc2
lotanaharoni <lotanaharoni@gmail.com>
2021-06-26 19:37:53 +0300

    Started MainActivity

--- app/src/main/java/com/example/postpc_ex8/MainActivity.java
+++ app/src/main/java/com/example/postpc_ex8/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.postpc_ex8;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText numberEntered;
+    Button calculateButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        numberEntered = findViewById(R.id.numberToEnter);
+        calculateButton = findViewById(R.id.calculateButton);
+
+        calculateButton.setOnClickListener(view -> {
+
+        });
     }
 }
