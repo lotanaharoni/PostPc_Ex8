032aedb82df547508e6058f7697e1b927696ad1a
lotanaharoni <lotanaharoni@gmail.com>
2021-06-27 15:11:56 +0300

    All in Adapter except onBundViewHolder

--- app/src/main/java/com/example/postpc_ex8/MyCalculatorAdapter.java
+++ app/src/main/java/com/example/postpc_ex8/MyCalculatorAdapter.java
@@ -1,16 +1,32 @@
 package com.example.postpc_ex8;
 
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class MyCalculatorAdapter extends RecyclerView.Adapter<MyCalculatorViewHolder> {
+
+    OnItemClickListener onDeleteClick;
+    OnItemClickListener onCancelClick;
+    ArrayList<CalculationItem> allItems;
+
+    public MyCalculatorAdapter(){
+        super();
+        this.allItems = new ArrayList<>();
+    }
 
     @NonNull
     @Override
     public MyCalculatorViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_one_item_calculation, parent, false);
+        return new MyCalculatorViewHolder(view);
     }
 
     @Override
@@ -20,6 +36,13 @@ public class MyCalculatorAdapter extends RecyclerView.Adapter<MyCalculatorViewHo
 
     @Override
     public int getItemCount() {
-        return 0;
+        return this.allItems.size();
+    }
+
+    public void setData(List<CalculationItem> newItems){
+        this.allItems.clear();
+        this.allItems.addAll(newItems);
+        Collections.sort(this.allItems);
+        notifyDataSetChanged();
     }
 }
