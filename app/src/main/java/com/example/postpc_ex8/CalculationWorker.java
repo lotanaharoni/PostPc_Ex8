209060823e4937b80a3f57bb0d9adefae1d744a9
lotanaharoni <lotanaharoni@gmail.com>
2021-06-28 11:23:16 +0300

    Fixed time time and added to CalculationWorker

--- app/src/main/java/com/example/postpc_ex8/CalculationWorker.java
+++ app/src/main/java/com/example/postpc_ex8/CalculationWorker.java
@@ -3,6 +3,7 @@ package com.example.postpc_ex8;
 import android.content.Context;
 
 import androidx.annotation.NonNull;
+import androidx.work.Data;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 
@@ -18,39 +19,54 @@ public class CalculationWorker extends Worker {
 
     public CalculationWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
         super(context, workerParams);
+        currentId = getInputData().getString("calculation_item_id");
+        // TODO: Initiate
     }
 
     @NonNull
     @Override
     public Result doWork() {
+        Data.Builder dataBuilder = new Data.Builder();
 
         if (number <= 0){
-            System.out.println("Wrong number");
+            dataBuilder.putLong("number", number);
+            dataBuilder.putString("reason", "illegal_number");
+            return Result.failure(dataBuilder.build());
         }
         else if (number == 1){
             root1 = number;
             root2 = 1;
-            // TODO: return
+            dataBuilder.putLong("number", number);
+            dataBuilder.putLong("root1", root1);
+            dataBuilder.putLong("root2", root2);
+            dataBuilder.putLong("progress", 0);
+            return Result.success(dataBuilder.build());
         }
         long timeStartMs = System.currentTimeMillis();
         double sqrtNumber = Math.sqrt(number);
         for (long i = 2; i <=sqrtNumber; i ++){
             long timePassed = System.currentTimeMillis() - timeStartMs;
             if (number % i == 0){
-                if (TimeUnit.MILLISECONDS.toSeconds(timePassed) <= 20) { // TODO: Change time
+                if (System.currentTimeMillis() - timeStartMs <= MAX_CALCULATION_TIME) { // TODO: Change time
                     root1 = i;
                     root2 = number / i;
-                    // sendSuccessResults(resultIntent, root1, root2, numberToCalculateRootsFor, TimeUnit.MILLISECONDS.toSeconds(timePassed));
-                    // return;
+                    dataBuilder.putLong("number", number);
+                    dataBuilder.putLong("root1", root1);
+                    dataBuilder.putLong("root2", root2);
+                    dataBuilder.putLong("progress", System.currentTimeMillis() - timeStartMs);
+                    return Result.success(dataBuilder.build());
                 }
             }
-            if (TimeUnit.MILLISECONDS.toSeconds(timePassed) > 20){ //TODO: Change time
-                // sendFailResults(resultIntent,timePassed, numberToCalculateRootsFor);
-               //  return;
+            if (System.currentTimeMillis() - timeStartMs > MAX_CALCULATION_TIME){ //TODO: Change time
+                dataBuilder.putLong("number", number);
+                dataBuilder.putString("reason", "illegal_time_to_calculate");
+                return Result.failure(dataBuilder.build());
             }
         }
-
-
-        return null;
+        dataBuilder.putLong("number", number);
+        dataBuilder.putLong("root1", 1);
+        dataBuilder.putLong("root2", number);
+        dataBuilder.putLong("progress", System.currentTimeMillis() - timeStartMs);
+        return Result.success(dataBuilder.build());
     }
 }
